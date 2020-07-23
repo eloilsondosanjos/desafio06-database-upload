2d375db8e26ba4fa92266f380d38362973b9ac0c
Eloilson Rocha <eloilson.ageedu@gmail.com>
2020-07-23 19:44:08 -0300

    Challenge Finished

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
+import cors from 'cors';
+
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
@@ -12,6 +14,7 @@ import createConnection from './database';
 createConnection();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
